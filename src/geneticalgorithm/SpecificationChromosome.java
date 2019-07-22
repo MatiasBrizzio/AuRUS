7e96a7c93fed6b5091527c543595e8ab3e104703
Renzo Gaston DEGIOVANNI <renzo.degiovanni@mp0185.uni.lux>
2019-07-22 14:03:04 +0200

    status of TLSF specification included to the chromosome.

--- src/geneticalgorithm/SpecificationChromosome.java
+++ src/geneticalgorithm/SpecificationChromosome.java
@@ -8,22 +8,25 @@ import tlsf.TLSF_Utils;
 
 public class SpecificationChromosome implements Chromosome<SpecificationChromosome>, Cloneable {
 	
-	// 			 /  ASSUMPTIONS  \
-	// UNKNOWN --				  --  UNREALIZABLE  --  REALIZABLE
-	//			 \  GUARANTEES   /
+	// 			 			/  ASSUMPTIONS  \
+	// UNKNOWN --  BOTTOM --		  		 -- CONTRADICTORY --  UNREALIZABLE  --  REALIZABLE
+	//			 			\  GUARANTEES   /
 	
 	// we distinguish the particular case when the specification is realizable
 	// just because the assumptions are unsatisfiable.
 	
 	public static enum SPEC_STATUS {
 		UNKNOWN, 		// UNKNOWN: the status of the specification has not been computed yet.
-		ASSUMPTIONS, 	// ASSUMPTIONS: the environment assumptions are consistent, but not the goals.
-		GUARANTEES, 	// GUARANTEES: the goals are consistent, but not the environment assumptions.
-		UNREALIZABLE, 	// UNREALIZABLE: the specification is consistent, but not realizable.
-		REALIZABLE;		// REALIZABLE: the specification is consistent and realizable.
+		BOTTOM,			// BOTTOM: both the assumptions and goals are unsatisfiable.
+		ASSUMPTIONS, 	// ASSUMPTIONS: the assumptions are consistent, but not the goals.
+		GUARANTEES, 	// GUARANTEES: the goals are consistent, but not the assumptions.
+		CONTRADICTORY,	// CONTRADICTORY: the assumptions and goals become unsatisfiable when are putted together. 
+		UNREALIZABLE, 	// UNREALIZABLE: the specification is satisfiable, but not realizable.
+		REALIZABLE;		// REALIZABLE: the specification is satisfiable and realizable.
 	
 		public boolean compatible (SPEC_STATUS other) {
 			if (this == UNKNOWN || other == UNKNOWN 
+				|| this == BOTTOM || other == BOTTOM 
 				|| (this == ASSUMPTIONS && other == ASSUMPTIONS) 
 				|| (this == GUARANTEES && other == GUARANTEES)
 				)
@@ -43,6 +46,7 @@ public class SpecificationChromosome implements Chromosome<SpecificationChromoso
 	
 	public SpecificationChromosome(Tlsf spec) {
 		this.spec = TlsfParser.parse(TLSF_Utils.toTLSF(spec));
+		//TODO: compute the state of spec
 		this.status = SPEC_STATUS.UNKNOWN;
 	}
 
