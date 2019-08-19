d001b53575d63783726ed5459ee203c538d29d30
rdegiovanni <rdegiovanni@dc.exa.unrc.edu.ar>
2019-08-19 19:12:45 -0300

    minot changes in the docjker file

--- run-syntcomp2019-casestudies.sh
+++ run-syntcomp2019-casestudies.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 for tlsf_file in `find examples/syntcomp2019/LTL-Real/ -iname "*unreal*" -print`; do
 	#dir="$(dirname $tlsf_file)"   # Returns "/from/hear/to"
-	#echo "Running $tlsf_file"
-	base="$(basename $tlsf_file)"  # Returns just "to"
-	filename="${base%.*}"
+	echo "Running $tlsf_file"
+	#base="$(basename $tlsf_file)"  # Returns just "to"
+	outname="${tlsf_file%.*}.out"
 		#echo `mkdir results/$dir/`
 	#echo `cp $res_file results/$dir/`
-	echo `./unreal-repair.sh $tlsf_file`
+	./unreal-repair.sh $tlsf_file > $outname
 	#echo $tlsf_file
 done
