95780260238b78eff55c412dd0bde930ae0d3bea
Renzo Gaston DEGIOVANNI <renzo.degiovanni@MP0185.fritz.box>
2019-08-20 18:05:41 +0200

    minor change to run strix in docker

--- docker/run-strix.sh
+++ docker/run-strix.sh
@@ -6,4 +6,4 @@ LTL=$(./syfco -f ltlxba -m fully $1)
 INS=$(./syfco  -f ltlxba --print-input-signals $1)
 OUTS=$(./syfco  -f ltlxba --print-output-signals $1)
 
-./bin/strix -f "$LTL" --ins "$INS" --outs "$OUTS" ${@:2}
+./bin/strix -r -f "$LTL" --ins "$INS" --outs "$OUTS" ${@:2}
