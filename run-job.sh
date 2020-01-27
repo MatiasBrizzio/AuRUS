9f78a01a282f0cd65c3ed0105d12e61919afe008
rdegiovanni <rdegiovanni@dc.exa.unrc.edu.ar>
2020-01-27 14:22:51 +0100

    scripts to run experiments in hpc

--- run-job.sh
+++ run-job.sh
@@ -12,13 +12,13 @@ module purge
 
 export BASEDIR=/home/users/rdegiovanni/unreal-repair
 
-export JAVA_HOME=/home/users/rdegiovani/envlib/java-11-oracle/
-export ANT_HOME=/home/users/rdegiovani/envlib/ant/
+export JAVA_HOME=/home/users/rdegiovanni/envlib/java-11-oracle/
+export ANT_HOME=/home/users/rdegiovanni/envlib/ant/
 export PATH=$PATH:$JAVA_HOME/bin:$ANT_HOME:bin
 
 pushd ${BASEDIR}
-CP="bin:lib/commons-math3-3.6.1.jar:lib/rltlconv.jar:lib/JFLAP-7.0_With_Source.jar:lib/owl-18.10-snapshot.jar:lib/ejml/ejml-core-0.34.jar:lib/ejml/ejml-cdense-0.34.jar:lib/ejml/ejml-ddense-0.34.jar:lib/ejml/ejml-fdense-0.34.jar:lib/ejml/ejml-simple-0.34.jar:lib/ejml/ejml-zdense-0.34.jar:lib/ejml/ejml-dsparse-0.34.jar:lib/ejml/ejml-experimental-0.34.jar:lib/ltl2buchi.jar"
-java -Xmx8g -Djava.library.path=/usr/local/lib -cp $CP main.Main "$@"
+ant compile
+./unreal-repair.sh "$@"
 
 popd
 
