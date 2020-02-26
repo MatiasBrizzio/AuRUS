d5119c10c58c403d7fb7929e1f0044026a0d2252
rdegiovanni <rdegiovanni@dc.exa.unrc.edu.ar>
2020-02-27 00:05:22 +0100

    script for running experiments

---
+++ run-all-together.sh
@@ -0,0 +1,65 @@
+#!/bin/bash -l
+
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -c 4
+#SBATCH --time=0-24:00:00
+#SBATCH -J Unreal-Repair
+#SBATCH --mail-user=renzo.degiovanni@uni.lu
+#SBATCH --mail-type=all
+
+module purge
+
+export BASEDIR=/home/users/rdegiovanni/unreal-repair
+
+export JAVA_HOME=/home/users/rdegiovanni/envlib/java-11-oracle/
+export ANT_HOME=/home/users/rdegiovanni/envlib/ant/
+export LIB_HOME=/home/users/rdegiovanni/envlib/lib/
+#export LD_LIBRARY_PATH=~/envlib/lib/:~/envlib/clib/:$LD_LIBRARY_PATH
+export PATH=$JAVA_HOME/bin:$ANT_HOME/bin:$LIB_HOME:$PATH
+
+module load tools/Singularity/3.5.2
+
+pushd ${BASEDIR}
+echo $JAVA_HOME
+echo $PATH
+
+echo $(java -version)
+echo $(javac -version)
+
+ant compile
+
+for K in {0..0}
+do
+echo "Running Minepump..."
+./unreal-repair.sh -Max=1000 -Gen=1000 -Pop=100 -k=20 -GATO=7200 -addA -onlyInputsA -ref=case-studies/minepump/genuine/minepump_fixed0.tlsf -ref=case-studies/minepump/genuine/minepump_fixed1.tlsf -ref=case-studies/minepump/genuine/minepump_fixed2.tlsf -out=result/minepump/minepump-genuine-$K case-studies/minepump/minepump.tlsf > result/minepump/minepump-genuine-$K.out 
+
+echo "Running Arbiter..."
+./unreal-repair.sh -Max=1000 -Gen=1000 -Pop=100 -k=20 -GATO=7200 -addA -onlyInputsA -ref=case-studies/arbiter/genuine/arbiter_fixed0.tlsf -ref=case-studies/arbiter/genuine/arbiter_fixed1.tlsf -out=result/arbiter/arbiter-genuine-$K case-studies/arbiter/arbiter.tlsf > result/arbiter/arbiter-genuine-$K.out  
+
+echo "Running Lily02..."
+./unreal-repair.sh -Max=1000 -Gen=1000 -Pop=100 -k=20 -GATO=7200 -addA -onlyInputsA -ref=case-studies/lily02/genuine/lilydemo02_fixed.tlsf -out=result/lilydemo02/lilydemo02-genuine-$K case-studies/lily02/lilydemo02.tlsf > result/lilydemo02/lilydemo02-genuine-$K.out 
+
+echo "Running RG1..."
+./unreal-repair.sh -Max=1000 -Gen=1000 -Pop=100 -k=20 -GATO=7200 -addA -onlyInputsA -ref=case-studies/RG1/genuine/RG1_fixed0.tlsf -ref=case-studies/RG1/genuine/RG1_fixed1.tlsf -ref=case-studies/RG1/genuine/RG1_fixed2.tlsf -ref=case-studies/RG1/genuine/RG1_fixed3.tlsf -out=result/RG1/RG1-genuine-$K case-studies/RG1/RG1.tlsf > result/RG1/RG1-genuine-$K.out 
+
+echo "Running RG2..."
+./unreal-repair.sh -Max=1000 -Gen=1000 -Pop=100 -k=20 -GATO=7200 -addA -onlyInputsA -ref=case-studies/RG2/genuine/RG2_fixed0.tlsf -ref=case-studies/RG2/genuine/RG2_fixed1.tlsf -out=result/RG2/RG2-genuine-$K case-studies/RG2/RG2.tlsf > result/RG2/RG2-genuine-$K.out 
+
+echo "Running Lift..."
+./unreal-repair.sh -Max=1000 -Gen=1000 -Pop=100 -k=20 -GATO=7200 -addA -onlyInputsA -ref=case-studies/lift/genuine/Lift_fixed0.tlsf -ref=case-studies/lift/genuine/Lift_fixed1.tlsf -out=result/Lift/Lift-genuine-$K case-studies/lift/Lift.tlsf > result/Lift/Lift-genuine-$K.out  
+
+echo "Running HumanoidLTL_458..."
+./unreal-repair.sh -Max=1000 -Gen=1000 -Pop=100 -k=20 -GATO=7200 -addA -onlyInputsA -ref=case-studies/HumanoidLTL_458/genuine/HumanoidLTL_460_Humanoid.tlsf -out=result/HumanoidLTL_458/HumanoidLTL_458-genuine-$K case-studies/HumanoidLTL_458/HumanoidLTL_458_Humanoid_fixed_unrealizable.tlsf > result/HumanoidLTL_458/HumanoidLTL_458-genuine-$K.out 
+
+#echo "Running HumanoidLTL_531..."
+#sbatch --output=result/HumanoidLTL_531/HumanoidLTL_531-genuine-$K.out ./run-job.sh -Max=1000 -Gen=1000 -Pop=100 -k=20 -GATO=7200 -addA -onlyInputsA -ref=case-studies/HumanoidLTL_531/genuine/HumanoidLTL_533_Humanoid.tlsf -out=result/HumanoidLTL_531/HumanoidLTL_531-genuine-$K case-studies/HumanoidLTL_531/HumanoidLTL_531_Humanoid_unrealizable.tlsf
+
+echo "Running GyroUnrealizable_Var1..."
+./unreal-repair.sh -Max=1000 -Gen=1000 -Pop=100 -k=20 -GATO=7200 -addA -onlyInputsA -ref=case-studies/GyroUnrealizable_Var1/genuine/GyroLTLVar3_702_GyroAspect.tlsf -out=result/GyroUnrealizable_Var1/GyroUnrealizable_Var1-genuine-$K case-studies/GyroUnrealizable_Var1/GyroUnrealizable_Var1_710_GyroAspect_unrealizable.tlsf > result/GyroUnrealizable_Var1/GyroUnrealizable_Var1-genuine-$K.out 
+
+echo "Running GyroUnrealizable_Var2..."
+./unreal-repair.sh -Max=1000 -Gen=1000 -Pop=100 -k=20 -GATO=7200 -addA -onlyInputsA -ref=case-studies/GyroUnrealizable_Var2/genuine/GyroLTLVar3_702_GyroAspect.tlsf -out=result/GyroUnrealizable_Var2/GyroUnrealizable_Var2-genuine-$K case-studies/GyroUnrealizable_Var2/GyroUnrealizable_Var2_710_GyroAspect_unrealizable.tlsf > result/GyroUnrealizable_Var2/GyroUnrealizable_Var2-genuine-$K.out 
+done
+
+popd
