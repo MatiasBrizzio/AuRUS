6fd8338cdb6d962b7310cbc1ce897dedb5f1e052
rdegiovanni <rdegiovanni@dc.exa.unrc.edu.ar>
2020-02-28 12:45:43 +0100

    minor change in read-result script

--- read-results.sh
+++ read-results.sh
@@ -9,19 +9,23 @@ DIR=$1-$K
 file=$DIR/out.txt
 
 GAtime=$(grep '^Time:' $file | grep -o ..........$)
-Sol=$(ls -1q $DIR/spec* | wc -l)
+#Sol=$(ls -1q $DIR/spec* | wc -l)
+Sol=$(grep "Num. of Solutions: " $file | grep -o ....$)
 BestFit=$(grep "Best fitness:" $file | grep -o ....$)
 AvgFit=$(grep "AVG fitness:" $file | grep -o ....$)
 
-GenSol=$(grep "Genuine Solutions found" $file | awk -F"," '{print NF-1}')
+#GenSol=$(grep "Genuine Solutions:" $file | awk -F"," '{print NF-1}')
+GenSol=$(grep "Genuine Solutions:" $file | grep -o ....$)
 GenBestFit=$(grep "Best Genuine fitness:" $file | grep -o ....$)
 GenAvgFit=$(grep "AVG Genuine fitness:" $file | grep -o ....$)
 
-WSol=$(grep "Weaker Solutions found" $file | awk -F"," '{print NF-1}')
+#WSol=$(grep "Weaker Solutions found" $file | awk -F"," '{print NF-1}')
+WSol=$(grep "Weaker Solutions:" $file | grep -o ....$)
 WBestFit=$(grep "Best Weaker fitness:" $file | grep -o ....$)
 WAvgFit=$(grep "AVG Weaker fitness:" $file | grep -o ....$)
 
-SSol=$(grep "Stronger Solutions found" $file | awk -F"," '{print NF-1}')
+#SSol=$(grep "Stronger Solutions found" $file | awk -F"," '{print NF-1}')
+SSol=$(grep "Stronger Solutions:" $file | grep -o ....$)
 SBestFit=$(grep "Best Stronger fitness:" $file | grep -o ....$)
 SAvgFit=$(grep "AVG Stronger fitness:" $file | grep -o ....$)
 
