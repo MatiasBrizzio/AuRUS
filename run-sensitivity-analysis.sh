bc2c1f1db01980dd547db89c8dda04d143661fa8
rdegiovanni <rdegiovanni@dc.exa.unrc.edu.ar>
2020-02-29 20:32:34 +0100

    script for running the sensitivy analysis

--- run-sensitivity-analysis.sh
+++ run-sensitivity-analysis.sh
@@ -5,9 +5,9 @@ CONFIG_SEM="-Max=1000 -Gen=1000 -Pop=100 -k=20 -GATO=7200 -addA -onlyInputsA -ge
 
 for K in {0..9}
 do
-	sbatch --job-name=$K-SReal --output=sensitivityresult/sensitivity-Real-$K.out ./run-sensitivity-analysis.sh $K "REAL" $CONFIG_REAL
-	sbatch --job-name=$K-SSyn --output=sensitivityresult/sensitivity-Syn-$K.out ./run-sensitivity-analysis.sh $K "SYN" $CONFIG_SYN
-	sbatch --job-name=$K-SSem --output=sensitivityresult/sensitivity-Sem-$K.out ./run-sensitivity-analysis.sh $K "SEM" $CONFIG_SEM
+	sbatch --job-name=$K-SReal --output=sensitivityresult/sensitivity-Real-$K.out ./run-all-sensitivity.sh $K "REAL" $CONFIG_REAL
+	sbatch --job-name=$K-SSyn --output=sensitivityresult/sensitivity-Syn-$K.out ./run-all-sensitivitys.sh $K "SYN" $CONFIG_SYN
+	sbatch --job-name=$K-SSem --output=sensitivityresult/sensitivity-Sem-$K.out ./run-all-sensitivity.sh $K "SEM" $CONFIG_SEM
 done
 
 
