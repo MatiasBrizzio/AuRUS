e01750fd2fc70babaaeacdc5eb9ee65960255138
matias <mathewb97@hotmail.com>
2024-01-12 09:50:36 +0100

    clean up

--- src/solvers/PotentiallyRealizabilityChecker.java
+++ src/solvers/PotentiallyRealizabilityChecker.java
@@ -23,22 +23,9 @@ public class PotentiallyRealizabilityChecker<S> {
 
     boolean isAcceptance = false;
     boolean noAcceptance = false;
-    //    private DMatrixRMaj T = null;
-//    private DMatrixRMaj I = null;
     private Automaton<S, EmersonLeiAcceptance> automaton = null;
-    //    private List<String> variables = null;
-//    private Map<S,Integer> states = null;
-    //public static int TIMEOUT = 120;
     private LabelledFormula formula;
     private List<String> input_vars = null;
-
-//    public boolean isStrongSatisfiable() {
-//        BigInteger numOfModels = count(Settings.MC_BOUND);
-//        BigInteger expectedNumOfModels =  (BigInteger.valueOf(2).pow(input_vars.size())).pow(Settings.MC_BOUND);
-//        System.out.printf("numOfModels: %d   expected: %d\n", numOfModels,expectedNumOfModels);
-//        return (numOfModels.compareTo(expectedNumOfModels) >= 0);
-//
-//    }
 
     public PotentiallyRealizabilityChecker(LabelledFormula formula) {
         this.formula = formula;
@@ -56,25 +43,6 @@ public class PotentiallyRealizabilityChecker<S> {
             System.err.println("PotentiallyRealizabilityChecker: ERROR while parsing. " + e.getMessage());
             System.err.println(formula);
         }
-//        System.out.println("Parsed...");
-//        System.out.println(HoaPrinter.toString(automaton, EnumSet.of(SIMPLE_TRANSITION_LABELS)));
-//        input_vars = new ArrayList<>(formula.player1Variables());
-//        variables = formula.variables();
-//        states = new HashMap<>();
-//        int index = 0;
-//        for(S s : automaton.states()) {
-//            states.put(s,index);
-//            index++;
-//        }
-        //From A0 we construct the (n + 1) x (n + 1) transfer matrix T. A0 has n + 1
-        //states s1, s2, . . . sn+1. The matrix entry Ti,j is the number of transitions from
-        //state si to state sj
-//        T = buildInputAutomatonTransferMatrix();
-//        setAcceptanceTransitions();
-//
-//		System.out.println("T: " + T.toString());
-//        int n = automaton.size();
-//        I = CommonOps_DDRM.identity(n);
     }
 
     private String parse() {
@@ -82,14 +50,6 @@ public class PotentiallyRealizabilityChecker<S> {
         DelagBuilder translator = new DelagBuilder(DefaultEnvironment.standard());
         automaton = (Automaton<S, EmersonLeiAcceptance>) translator.apply(formula);
         var environment = DefaultEnvironment.standard();
-//        var translator = new LTL2DPAFunction(environment, EnumSet.of(
-//                OPTIMISE_INITIAL_STATE,
-//                COMPLEMENT_CONSTRUCTION,
-//                GREEDY,
-//                COMPRESS_COLOURS));
-//        LTL2DPAFunction.RECOMMENDED_SYMMETRIC_CONFIG);
-
-//        automaton = (Automaton<S, ParityAcceptance>) translator.apply(formula);
         return "OK";
     }
 
@@ -236,219 +196,8 @@ public class PotentiallyRealizabilityChecker<S> {
             }
         }
 
-//        System.out.printf("states = %d \n", automaton.size());
-//        System.out.printf("undesiredStates = %d \n", undesiredStates.size());
-////            if(! numOfInputEvents.equals(BigInteger.valueOf(numOfAcceptanceTransition))) {
-//
-//        if (undesiredStates.contains(automaton.onlyInitialState()))
-//            return false;
-//
-//        //check if there is a (controlable) way to avoid reaching the bad state
-//        for (S bad_state : undesiredStates) {
-//            for(S predecessor : automaton.predecessors(bad_state)) {
-//                if (undesiredStates.contains(predecessor))
-//                    continue;
-//
-//                Map<Edge<S>, ValuationSet> predecessor_edges = automaton.edgeMap(predecessor);
-//                if(!predecessor_edges.isEmpty()) {
-//                    //create valuation factory
-//                    Environment env = DefaultEnvironment.standard();
-//                    FactorySupplier factorySupplier = env.factorySupplier();
-//                    ValuationSetFactory inputFactory = factorySupplier.getValuationSetFactory(new ArrayList<>(input_vars));
-//
-//                    noAcceptance = false;
-//                    inputFactory.universe().forEach(inputBitSet -> {
-//                        if (noAcceptance)
-//                            return;
-//
-//                        isAcceptance = false;
-//                        predecessor_edges.forEach((predecessor_edge, valuation) -> {
-//                            if (isAcceptance)
-//                                return;
-//                            if (predecessor_edge.successor().equals(bad_state))
-//                                return;
-//                            valuation.forEach(bitSet -> {
-//                                BitSet bs1 = bitSet.get(0, input_vars.size());
-//                                //                            System.out.println(inputBitSet + ": "+ bitSet + "  ->  " + bs1);
-//                                if (bs1.equals(inputBitSet)) {
-//                                    //check if it is an acceptance transition
-//                                    IntArrayList acceptanceSets = new IntArrayList();
-//                                    if (predecessor_edge.acceptanceSetIterator().hasNext())
-//                                        predecessor_edge.acceptanceSetIterator().forEachRemaining((IntConsumer) acceptanceSets::add);
-//                                    if (accConditionIsSatisfied(automaton.acceptance().booleanExpression(), acceptanceSets)) {
-//                                        isAcceptance = true;
-//                                    }
-//                                }
-//                            });
-//                        });
-//                        if (!isAcceptance) {
-//                            noAcceptance = true;
-//                        }
-//                    });
-//                    if (noAcceptance) {
-////                        System.out.printf("ERROR: state[%d] = %s\n", states.get(bad_state), bad_state);
-//                        return false;
-//                    }
-//                }
-//            }
-//        }
         return true;
     }
-
-
-//    public  BigInteger count(int bound){
-//        //We compute uTkv, where u is the row vector such that ui = 1 if and only if i is the start state and 0 otherwise,
-//        // and v is the column vector where vi = 1 if and only if i is an accepting state and 0 otherwise.
-//        if (automaton == null || automaton.size() == 0)
-//            return BigInteger.ZERO;
-//        int N = automaton.size();
-//        //set initial states
-//        DMatrixRMaj u = new DMatrixRMaj(1,N+1);
-//        for(S is : automaton.initialStates()) {
-//            u.set(0, states.get(is),1);
-//        }
-//        System.out.println("Initial: " + u.toString());
-//        //set final states
-//        DMatrixRMaj v = new DMatrixRMaj(N+1,1);
-//        v.set(N, 0, 1);
-//
-////        Set<S> final_states = new HashSet<>();
-////        for (S s : automaton.states()) {
-////            Set<Edge<S>> edges = automaton.edges(s);
-////            for(Edge<S> edge : edges) {
-////                //check if it is an acceptance transition
-////                IntArrayList acceptanceSets = new IntArrayList();
-////                if (edge.acceptanceSetIterator().hasNext())
-////                    edge.acceptanceSetIterator().forEachRemaining((IntConsumer) acceptanceSets::add);
-////                if (accConditionIsSatisfied(automaton.acceptance().booleanExpression(), acceptanceSets)) {
-////                    final_states.add(edge.successor());
-////                }
-////            }
-////        }
-////        for(S fs : final_states) {
-////            v.set(states.get(fs), 0, 1);
-////        }
-//        System.out.println("Final: " + v.toString());
-//        // count models
-//
-//        DMatrixRMaj T_res = T.copy();
-//
-//        for(int i=1; i<bound; i++) {
-//            long initialTime = System.currentTimeMillis();
-//            DMatrixRMaj T_i = T.copy();
-//            DMatrixRMaj T_aux = T_res.copy();
-//            CommonOps_DDRM.mult(T_aux, T_i, T_res);
-////			System.out.println(i + ": " + T_res.toString());
-//            //check for timeout
-//            long currentTime = System.currentTimeMillis();
-//            long totalTime = currentTime-initialTime;
-//            int min = (int) (totalTime)/60000;
-//            int sec = (int) (totalTime - min*60000)/1000;
-//            if (sec > TIMEOUT) {
-//                System.out.print("TO ");
-//                return BigInteger.ZERO;
-//            }
-//        }
-//        DMatrixRMaj reachable = new DMatrixRMaj(1,N+1);
-//        CommonOps_DDRM.mult(u, T_res, reachable);
-////		System.out.println("reachable: " + reachable.toString());
-//        DMatrixRMaj result = new DMatrixRMaj(1,1);
-//        CommonOps_DDRM.mult(reachable,v,result);
-////		System.out.println("result: " + result.toString());
-//        long value = (long)result.get(0,0);
-//        BigInteger count = BigInteger.valueOf(value);
-//        return count;
-//    }
-//
-//    long transitions = 0;
-//    boolean existsInputTransition = false;
-//    public DMatrixRMaj buildInputAutomatonTransferMatrix() {
-//        int n = automaton.size();
-//        //add one sink state thar represents the acceptance state
-//        DMatrixRMaj M = new DMatrixRMaj(n+1,n+1);
-//        for (S si : automaton.states()){
-//            Map<Edge<S>, ValuationSet> edges = automaton.edgeMap(si);
-//            for(S sj : automaton.successors(si)) {
-//                transitions = 0;
-//                //create valuation factory
-//                Environment env = DefaultEnvironment.standard();
-//                FactorySupplier factorySupplier = env.factorySupplier();
-//                ValuationSetFactory inputFactory = factorySupplier.getValuationSetFactory(new ArrayList<>(input_vars));
-//                inputFactory.universe().forEach(inputBitSet -> {
-//                    existsInputTransition = false;
-//                    edges.forEach((edge,valuation) -> {
-//                        if (!existsInputTransition && edge.successor().equals(sj)) {
-//                            //                    //create valuation factory
-//                            //                    Environment env2 = DefaultEnvironment.standard();
-//                            //                    FactorySupplier factorySupplier2 = env2.factorySupplier();
-//                            //                    ValuationSetFactory allFactory = factorySupplier2.getValuationSetFactory(variables);
-////                            System.out.printf("M[%d,%d]\n", states.get(si),states.get(sj));
-//                            valuation.forEach(bitSet -> {
-//                                if (existsInputTransition)
-//                                    return;
-//                                BitSet bs1 = bitSet.get(0,input_vars.size());
-////                                System.out.println(inputBitSet + ": "+ bitSet + "  ->  " + bs1);
-//                                if (bs1.equals(inputBitSet)) {
-////                                    System.out.println("found....");
-//                                    existsInputTransition = true;
-//                                }
-//                            });
-//                        }
-//                    });
-//                    if (existsInputTransition)
-//                        transitions++;
-//                });
-//                System.out.printf("M[%d,%d] = %d \n", states.get(si),states.get(sj),transitions);
-//                M.set(states.get(si), states.get(sj), transitions);
-//            }
-//        }
-//
-//
-//        return M;
-//    }
-//
-//    boolean existsAcceptanceTransition = false;
-//    long acceptanceTransitions = 0;
-//    public void setAcceptanceTransitions() {
-//        int n = automaton.size();
-//        for (S si : automaton.states()){
-//            acceptanceTransitions = 0;
-//            Map<Edge<S>, ValuationSet> edges = automaton.edgeMap(si);
-//            //create valuation factory
-//            Environment env = DefaultEnvironment.standard();
-//            FactorySupplier factorySupplier = env.factorySupplier();
-//            ValuationSetFactory inputFactory = factorySupplier.getValuationSetFactory(new ArrayList<>(input_vars));
-//            inputFactory.universe().forEach(inputBitSet -> {
-//                existsAcceptanceTransition = false;
-//                edges.forEach((edge,valuation) -> {
-//                    if (!existsAcceptanceTransition) {
-//                        valuation.forEach(bitSet -> {
-//                            if (existsAcceptanceTransition)
-//                                return;
-//                            BitSet bs1 = bitSet.get(0,input_vars.size());
-////                            System.out.println(inputBitSet + ": "+ bitSet + "  ->  " + bs1);
-//                            if (bs1.equals(inputBitSet)) {
-//                                //check if it is an acceptance transition
-//                                IntArrayList acceptanceSets = new IntArrayList();
-//                                if (edge.acceptanceSetIterator().hasNext())
-//                                    edge.acceptanceSetIterator().forEachRemaining((IntConsumer) acceptanceSets::add);
-//                                if (accConditionIsSatisfied(automaton.acceptance().booleanExpression(), acceptanceSets)) {
-//                                    existsAcceptanceTransition = true;
-//                                }
-//                            }
-//                        });
-//                    }
-//
-//                });
-//                if (existsAcceptanceTransition)
-//                    acceptanceTransitions++;
-//            });
-//            System.out.printf("M[%d,%d] = %d \n", states.get(si),n,acceptanceTransitions);
-//            T.set(states.get(si), n, acceptanceTransitions);
-//        }
-////        T.set(n, n, 1);
-//    }
-
 
     public boolean accConditionIsSatisfied(BooleanExpression<AtomAcceptance> acceptanceCondition, IntArrayList acceptanceSets) {
         boolean accConditionSatisfied = false;
--- src/solvers/PreciseLTLModelCounter.java
+++ src/solvers/PreciseLTLModelCounter.java
@@ -1,9 +1,6 @@
 package solvers;
 
-import owl.ltl.Disjunction;
 import owl.ltl.Formula;
-import owl.ltl.Literal;
-import owl.ltl.parser.LtlParser;
 
 import java.io.*;
 import java.math.BigInteger;
@@ -15,9 +12,6 @@ public class PreciseLTLModelCounter {
     public final String BASENAME = "lib/ltl-model-counter/result/numofmodels";
     public final String INFILE = BASENAME + ".ltl";
     public int BOUND = 5;
-    public final String PROP_FILE = BASENAME + "-k" + BOUND + ".sat";
-    public final String PROP_CNF_FILE = BASENAME + "-k" + BOUND + ".cnf";
-
     public int numOfTimeout = 0;
     public int numOfError = 0;
     public int numOfCalls = 0;
@@ -56,12 +50,10 @@ public class PreciseLTLModelCounter {
 
     public BigInteger count(String formula, List<String> variables) throws IOException, InterruptedException {
         numOfCalls++;
-//		System.out.println(formula);
         Process p = null;
 
         // make formula file
         if (formula != null && variables != null) {
-//			toCNF(formula, variables);
             File f = new File(INFILE);
             FileWriter fw = new FileWriter(f);
             for (String v : variables)
@@ -72,13 +64,6 @@ public class PreciseLTLModelCounter {
             // run counting command
             String cmd = getCommand();
             p = Runtime.getRuntime().exec(cmd);
-//	    	OutputStream out = p.getOutputStream();
-//	    	OutputStreamWriter bufout = new OutputStreamWriter(out);
-//	    	BufferedWriter bufferedwriter = new BufferedWriter(bufout, formula.getBytes().length);
-//    		bufferedwriter.write(formula);
-//	    	bufferedwriter.close();
-//	    	bufout.close();
-//	    	out.close();
         }
 
         boolean timeout = false;
@@ -144,64 +129,6 @@ public class PreciseLTLModelCounter {
         if (os != null) os.close();
 
         return numOfModels;
-    }
-
-    private void toCNF(String formula, List<String> variables) throws IOException, InterruptedException {
-        File f = new File(INFILE);
-        FileWriter fw = new FileWriter(f);
-        for (String v : variables)
-            fw.append(v).append("\n");
-        fw.append("###\n");
-        fw.append(formula).append("\n");
-        fw.close();
-
-        // run ltl2pl command
-        String cmdltl2pl = getCommandLTL2PL();
-        Process p = Runtime.getRuntime().exec(cmdltl2pl);
-        p.waitFor(TIMEOUT, TimeUnit.SECONDS);
-
-        //read all CNF clauses
-        List<String> vars = new LinkedList<>(); //BMC variables
-        for (String v : variables) {
-            for (int i = 0; i < BOUND + 1; i++)
-                vars.add(v + i);
-        }
-        for (int i = 0; i < BOUND; i++)
-            vars.add("l" + i);
-
-        List<Formula> clauses = new LinkedList<>();
-        BufferedReader br = new BufferedReader(new FileReader(PROP_FILE));
-        String prop_str = br.readLine();
-        while (prop_str != null && !prop_str.equals("")) {
-            Formula clause = LtlParser.syntax(prop_str, vars);
-            clauses.add(clause);
-            prop_str = br.readLine();
-        }
-        br.close();
-//		System.out.println(prop_str);
-        fw = new FileWriter(new File(PROP_CNF_FILE));
-        fw.append("p cnf ").append(String.valueOf(vars.size())).append(" ").append(String.valueOf(clauses.size())).append("\n");
-        for (Formula clause : clauses)
-            fw.append(cnfStr(clause)).append("\n");
-//		System.out.println(cnf_formula.toString());
-//
-//		fw.append(cnf_formula.toString());
-        fw.close();
-    }
-
-    private String cnfStr(Formula f) {
-        if (!(f instanceof Disjunction))
-            throw new IllegalArgumentException("LTLModelCounter: formula is not in cnf format");
-        Disjunction clause = (Disjunction) f;
-        StringBuilder cnf = new StringBuilder();
-        for (Formula c : clause.children()) {
-            Literal l = (Literal) c;
-            if (l.isNegated()) cnf.append("-");
-            cnf.append(l.getAtom() + 1);
-            cnf.append(" ");
-        }
-        cnf.append("0");
-        return cnf.toString();
     }
 
     public enum MODEL_COUNTER {
